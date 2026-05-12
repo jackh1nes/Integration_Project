fb5cd040a21f1d12a8f32897a38bdb1275494adb
Sebestyen <sebiuray@gmail.com>
2026-05-12 17:46:55 +0200

    finished SYSYEM ID LETS GO

--- inter_param_fun.m
+++ inter_param_fun.m
@@ -1,4 +1,4 @@
-function [dx, y] = inter_param_fun(t,x,u,a2, varargin)
+function [dx, y] = inter_param_fun(t,x,u,a1,a2,a3, a4, a5, varargin)
 
 phi_dot = x(1);
 theta = x(2);
@@ -8,9 +8,9 @@ i = u(1);
 
 dx = zeros(3,1);
 
-dx(1) = (0.9904-1)/0.01 * phi_dot + 413.34 * i;
+dx(1) = -a1 * phi_dot + a5 * i;
 dx(2) = theta_dot;
-dx(3) = a2*phi_dot - 0.033262024/0.01 * sin(theta) + (0.99812133-1)/0.01 * theta_dot;
+dx(3) = a2*phi_dot - a3 * sin(theta) - a4 * theta_dot;
 
 y = [phi_dot; theta];
 end
